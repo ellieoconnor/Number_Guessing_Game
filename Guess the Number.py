0e94c6647175aa426a857b769f5cdea4d30b5b07
Ellie O'Connor <87724044+ellieoconnor@users.noreply.github.com>
2021-08-18 08:07:33 -0400

    Update Guess the Number.py
    Trying to get the program to state the number of guesses when the player wins.
    Also, I want the program to print a specific statement when the user loses/runs out of guesses.
    The user gets three chances to guess the correct number.

--- Guess the Number.py
+++ Guess the Number.py
@@ -1,24 +1,33 @@
 import random
+#import time...then have a wait time after line 10
 number = random.randint(1, 10)
 guess = ''
+number_of_guesses = 0
 
 print("Hello! Welcome to Guess the Number!")
-name = input("\nWhat is your name? ")
-print("\nExcellent " + name + "! Let's play!")
 print("\nYou may type 'quit' to quit the game at anytime.\n")
+name = input("\nWhat is your name? ")
+print("\nOkay " + name + "! Let's play! \nI am thinking of a number between 1 and 10.")
+
+print("I have my number.")
 
 
-while guess != 'quit':
-    guess = input("\nPlease guess a number between 1 and 10: ")
+while number_of_guesses < 3:
+    guess = input("\nGuess a number between 1 and 10: ")
     guess = int(guess)
+    number_of_guesses += 1
+
+    if guess == 'q':
+        print("Thank you for playing. Goodbye!")
+        break
 
     if guess > number:
         print("Your guess is too high")
-
-    if guess < number:
+    elif guess < number:
         print("Your guess is too low.")
+    elif guess == number:
+        print("\nYou guessed the number in " + str(number_of_guesses) + ' tries!')
+    elif number_of_guesses > 3:
+        print("\nYou did not guess the number. The number was " + str(number))
 
-    if guess == number:
-        break
-    
-print('You are correct! \nYou guessed the mystery number: ' + str(number))
+print("Thank you again for playing.")
